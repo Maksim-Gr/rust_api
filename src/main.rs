2243fcb2b6fb37dab053e492935c527be1dd53a4
Makism-Gr <maksim.g.grishchenko@gmail.com>
2025-02-19 13:11:26 +0300

    format files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use tokio::net::TcpListener;
 use rust_api::run;
-
-
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Could not bind");
-    run(listener)?.await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,8 @@
-use std::net::TcpListener;
 use rust_api;
-
-
-
+use std::net::TcpListener;
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = rust_api::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
@@ -30,18 +26,39 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
 
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
 
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
 
-
-
-
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad request when payload was {}.",
+            error_message
+        )
+    }
+}
 
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
-
 
     let response = client
         .get(&format!("{}/health_check", &address))
@@ -51,5 +68,4 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-
 }
