2e4e738a217cbe86e0ae100f1b74684886d7c7c8
Makism-Gr <maksim.g.grishchenko@gmail.com>
2025-02-19 10:29:45 +0300

    tests: add test for form validation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -14,6 +14,30 @@ fn spawn_app() -> String {
 }
 
 #[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+
+
+
+
+
+
+
+#[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
