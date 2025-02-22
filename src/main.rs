9a6fc3d84fabcbcb72df08d593cf334ad07a0ff2
Makism-Gr <maksim.g.grishchenko@gmail.com>
2025-02-22 20:51:36 +0300

    add ARC connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use rust_api::configuration::get_configuration;
 use rust_api::startup::run;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string());
     let address = format!("127.0.0.1{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -35,7 +35,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&mut connection).await
+        .fetch_one(&mut connection)
+        .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
