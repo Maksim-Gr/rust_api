b499932280d5e7c479c8b00972d7c463fe189929
Maksim Grishchenko <grischenko1988@gmail.com>
2025-03-17 14:51:38 +0300

    chore: add telemetry mod

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 use env_logger::Env;
 use rust_api::configuration::get_configuration;
 use rust_api::startup::run;
+use rust_api::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
+use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("rust_api".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("rust_api".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
@@ -26,5 +22,6 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection)?.await
+    run(listener, connection)?.await?;
+    Ok(())
 }
