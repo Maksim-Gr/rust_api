15497f835cd87e452b9cc74975516783ca8b9344
Maksim Grishchenko <grischenko1988@gmail.com>
2025-03-26 15:05:25 +0300

    feature: add new pgpool for config

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rust_api::configuration::get_configuration;
 use rust_api::startup::run;
 use rust_api::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,16 +12,15 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection)?.await?;
+    run(listener, connection_pool)?.await?;
     Ok(())
 }
