33a9190ece6f8a3316df6f6c09dd5e5a38d31ab8
Maksim Grishchenko <grischenko1988@gmail.com>
2025-03-29 14:06:08 +0300

    chore: add ssl to db migration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 use config::Environment;
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
@@ -23,6 +23,7 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
@@ -30,6 +31,12 @@ impl DatabaseSettings {
         self.without_db().database(&self.database_name)
     }
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
